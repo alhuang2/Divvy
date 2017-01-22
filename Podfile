8dce1e158c273f59f7d06083f3ff6d5388d3b825
alhuang2 <alstonhuang@Alstons-Mac.local>
2017-01-21 23:40:38 -0800

    First commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'divvy' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for divvy
+
+  target 'divvyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'divvyUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+    pod 'TesseractOCRiOS', '4.0.0'
+end
--- divvy/ViewController.swift
+++ divvy/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import TesseractOCR
+class ViewController: UIViewController, G8TesseractDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    @IBAction func photoLibrary(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        image.allowsEditing = false
+        self.present(image, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            myImageView.image = image
+        }
+        else{}
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func convert(_ sender: Any) {
+        if let tesseract = G8Tesseract(language: "eng"){
+            tesseract.delegate = self
+            tesseract.image =  myImageView.image?.g8_blackAndWhite()
+            //UIImage(named: "Outline")?.g8_blackAndWhite()
+            tesseract.recognize()
+            textView.text = tesseract.recognizedText
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
